0f1dbaca70487483131ac608f17ddd86ef0fb5c1
roheetmeister <73239386+roheetmeister@users.noreply.github.com>
2021-09-19 12:47:00 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,4 +1,5 @@
 # Declare the data source
+#Terraform uses data sources to fetch information from cloud provider APIs, such as disk image IDs, or information about the rest of your infrastructure through the outputs of other Terraform configurations.
 data "aws_availability_zones" "available" {
   state = "available"
 
